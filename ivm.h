6c3c447c807c52b174043a7334ee14683a352872
Alan <alan2here@gmail.com>
2013-06-29 19:33:01 +0100

    Tidy
    I've tried not to be too obtrusive, but have renamed members with
    comment lines that would be redundant had the names been subtlety
    different.

--- ivm.h
+++ ivm.h
@@ -11,19 +11,19 @@ class IVM
 		// returns the name of this vm
 		virtual std::string getName() = 0;
 		
-		// get how many instructions this VM supports
-		virtual int getInstructionCount() = 0;
+        virtual int supportedInstructionCount() = 0;
 		
 		// load a program
-		virtual void loadProgram(const std::vector<int>& program) = 0;
-		
-		// format the program as a string
-		virtual std::string formatProgram(const std::vector<int>& program) = 0;
-		
-		// run the program
-		// returns false if the program failed to produce output for any reason
+        virtual void loadProgram(const std::vector<int>& program) = 0;
+
+        virtual std::string returnStringForm(const std::vector<int>& program) = 0;
+
+        virtual std::vector<int>& getCodeReadOnly();
+
+        // returns false if the program failed to produce output
 		virtual bool run(float seed, float* output, int maxOps) = 0;
-		
-		// returns information about why the program failed to produce output
+        // why does this need a seed?
+
+        // why the program failed to produce output
 		virtual std::string getLastError() = 0;
 };
