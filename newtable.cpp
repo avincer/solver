2f7b520063c0e5daee0957bbad82de8cc9ea8b56
Alan <alan2here@gmail.com>
2013-06-29 19:35:11 +0100

    Refactor of NewtAble
    Now works with IVM, or at least should do, I had trouble compiling
    because of other code. We can merge branches now and all work on the
    same branch!

--- newtable.cpp
+++ newtable.cpp
@@ -10,87 +10,73 @@
 
 namespace VMs
 {
-    class IInst
-    {
-        unsigned short ways_to_expand();
-
-        void expand(unsigned short n);
-    };
-
-    namespace pileUp
-    {
-        // dosn't need an instructions, default behaviour used
-
-        class VM : public IVM
-        {
-        };
-    }
-
     namespace newtAble
     {
-        class instructions : public IInst
-        { // needs an interface, put at the top of this file
-            public:
-                friend class VM;
-
-                instructions() {
-                    ways = 11;}
-
-                unsigned short ways_to_expand() {
-                    return ways;}
-
-                void expand(unsigned short n)
-                {
-                    // 11 instructions
-                    store.insert(store.begin() + (n % 11), n);
-                    ways = (store.size() + 1) * 11;
-                }
-
-            private:
-                std::vector<unsigned short> store;
-                unsigned short ways;
-        };
-
         class VM : public IVM
         {
             public:
-                unsigned int
-                    most_run_time_per_output,
-                    largest_code_runTime_size;
-
                 VM() {
                     code_pos = 0;
-                    latest_output_p = nullptr;
                     most_run_time_per_output = 100;
                     largest_code_runTime_size = 40;
-                    debug_info = false;}
+                    runTime_code_output = false;}
 
-                void set_code(
-                    instructions code) {
-                    this->code = code;
+                std::string getName() {
+                    return "newtAble";}
+
+                int supportedInstructionCount() {
+                    return 11;}
+
+                void loadProgram(
+                    const std::vector<int>& program) {
+                    code = program;
                     reset();}
 
-                void reset() {
-                    if (latest_output_p != nullptr) {
-                        delete latest_output_p;
-                        latest_output_p = nullptr;}
-                    code_pos = 0;
-                    first_value = true;
-                    can_run_again_p = code.store.size() != 0;}
-
-                bool can_run_again() {
-                    return can_run_again_p;}
-
-                unsigned short* latest_output() {
-                    return latest_output_p;}
-
-                void run()
+                std::string returnStringForm(
+                    const std::vector<int>& program)
                 {
-                    bool cont = can_run_again_p;
+                    std::string str;
+                    std::vector<int>::size_type
+                        code_length = program.size();
+                    for (unsigned short n = 0; n < code_length; n++)
+                        str += instruction_names[program[n]] +
+                            (n < code_length - 1 ? " ": "");
+                    VM tmp();
+                    tmp.loadProgram(program);
+                    str += ", run time: ";
+                    float* output;
+                    tmp.runTime_code_output = true;
+                    tmp.run(0, output, 100000);
+                    str += tmp.last_runTime_code();
+                    return str;
+                }
+
+                std::string last_runTime_code() {
+                    return last_runTime_code_p;}
+
+                void reset() {
+                    code_pos = 0;
+                    first_value = true;}
+
+                std::string getLastError() {
+                    return last_error;}
+
+                std::vector<int>& getCodeReadOnly() {
+                    return code;}
+
+                bool runTime_code_output;
+
+                bool run(float seed, float* output, int maxOps)
+                {
+                    unsigned int most_run_time_per_output = maxOps;
+                    bool cont = true,
+                        ok_run = true;
+                    last_error = "";
+                    last_runTime_code_p = "";
                     if (first_value)
                         code_runTime_copy = code;
                     size_t code_runTime_size =
-                        code_runTime_copy.store.size();
+                        code_runTime_copy.size();
                     unsigned short
                         instruction,
                         run_time_this_output = 0;
@@ -98,185 +84,168 @@ namespace VMs
                         instructions = 11;
                     while(cont)
                     {
-                        instruction = code_runTime_copy.store[code_pos] % instructions;
-                        if(debug_info)
-                            std::cout<<code_pos<<", ";
+                        instruction = code_runTime_copy[
+                            code_pos] % instructions;
                         code_pos = (code_pos + 1) % code_runTime_size;
                         switch(instruction)
                         {
                             case 0:
-                                if(debug_info)
-                                    std::cout<<"end";
+                                if(runTime_code_output)
+                                    last_runTime_code_p += "end";
                                 cont = false;
-                                can_run_again_p = false;
-                                if (latest_output_p != nullptr)
-                                {
-                                    delete latest_output_p;
-                                    latest_output_p = nullptr;}
+                                ok_run = false;
+                                last_error = "program complete";
                             break;
                             case 1:
-                                if(debug_info)
-                                    std::cout<<"goto p1";
-                                code_pos = code_runTime_copy.store[
+                                if(runTime_code_output)
+                                    last_runTime_code_p += "goto p1";
+                                code_pos = code_runTime_copy[
                                     code_pos % code_runTime_size];
                             break;
                             case 2:
-                                if(debug_info)
-                                    std::cout<<"if value at p1 % 2 = 0 then goto p2";
-                                if(code_runTime_copy.store[
-                                    code_runTime_copy.store[code_pos]
+                                if(runTime_code_output)
+                                    last_runTime_code_p += "if @p1 % 2 = 0 then goto p2";
+                                if(code_runTime_copy[
+                                    code_runTime_copy[code_pos]
                                         % code_runTime_size] % 2 == 0)
-                                    code_pos = code_runTime_copy.store[
+                                    code_pos = code_runTime_copy[
                                         (code_pos + 1) % code_runTime_size];
                                 else
                                     code_pos += 2;
                             break;
                             case 3:
-                                if(debug_info)
-                                    std::cout<<"value at p1 = value at p2";
-                                code_runTime_copy.store[
-                                    code_runTime_copy.store[code_pos]
+                                if(runTime_code_output)
+                                    last_runTime_code_p += "@p1 = @p2";
+                                code_runTime_copy[
+                                    code_runTime_copy[code_pos]
                                     % code_runTime_size] =
-                                        code_runTime_copy.store[
-                                            code_runTime_copy.store[
+                                        code_runTime_copy[
+                                            code_runTime_copy[
                                                 (code_pos + 1) % code_runTime_size]
                                             % code_runTime_size];
                                 code_pos += 2;
                             break;
                             case 4:
-                                if(debug_info)
-                                    std::cout<<"value at p1 = value at p2 + value at p3";
-                                code_runTime_copy.store[
-                                    code_runTime_copy.store[code_pos] % code_runTime_size] = (
-                                        code_runTime_copy.store[
-                                            code_runTime_copy.store[
+                                if(runTime_code_output)
+                                    last_runTime_code_p += "@p1 = @p2 + @p3";
+                                code_runTime_copy[
+                                    code_runTime_copy[code_pos] % code_runTime_size] = (
+                                        code_runTime_copy[
+                                            code_runTime_copy[
                                             (code_pos + 1) % code_runTime_size]
                                             % code_runTime_size] +
-                                        code_runTime_copy.store[
-                                            code_runTime_copy.store[
+                                        code_runTime_copy[
+                                            code_runTime_copy[
                                                 (code_pos + 2) % code_runTime_size] % code_runTime_size]
                                         ) % USHRT_MAX;
                                 code_pos += 3;
                             break;
                             case 5:
                             {
-                                if(debug_info)
-                                    std::cout<<"value at p1 = value at p2 - value at p3";
+                                if(runTime_code_output)
+                                    last_runTime_code_p += "@p1 = @p2 - @p3";
                                 long v1 =
-                                (long)code_runTime_copy.store[
-                                    code_runTime_copy.store[
+                                (long)code_runTime_copy[
+                                    code_runTime_copy[
                                         (code_pos + 1) % code_runTime_size]
                                     % code_runTime_size] -
-                                    code_runTime_copy.store[
-                                        code_runTime_copy.store[
+                                    code_runTime_copy[
+                                        code_runTime_copy[
                                             (code_pos + 2) % code_runTime_size]
                                         % code_runTime_size];
-                                code_runTime_copy.store[
-                                    code_runTime_copy.store[code_pos]
+                                code_runTime_copy[
+                                    code_runTime_copy[code_pos]
                                         % code_runTime_size] = abs(v1) % USHRT_MAX;
                                 code_pos += 3;
                             }
                             break;
                             case 6:
                             {
-                                if(debug_info)
-                                    std::cout<<"toggle value at p1";
-                                size_t v1 = code_runTime_copy.store[code_pos]
+                                if(runTime_code_output)
+                                    last_runTime_code_p += "toggle @p1";
+                                size_t v1 = code_runTime_copy[code_pos]
                                     % code_runTime_size;
-                                unsigned short v2 = code_runTime_copy.store[v1];
-                                if(v2 == 0) {code_runTime_copy.store[v1] = 1;}
-                                else {code_runTime_copy.store[v1] = 0;}
+                                unsigned short v2 = code_runTime_copy[v1];
+                                if(v2 == 0) {code_runTime_copy[v1] = 1;}
+                                else {code_runTime_copy[v1] = 0;}
                                 code_pos++;
                             }
                             break;
                             case 7:
-                                if(debug_info)
-                                    std::cout<<"output value at p1";
-                                if (latest_output_p != nullptr)
-                                    delete latest_output_p;
-                                latest_output_p = new unsigned short(
-                                    code_runTime_copy.store[
-                                        code_runTime_copy.store[code_pos]
+                                if(runTime_code_output)
+                                    last_runTime_code_p += "output @p1";
+                                output = new float(
+                                    code_runTime_copy[
+                                        code_runTime_copy[code_pos]
                                         % code_runTime_size]);
                                 code_pos++;
                                 cont = false;
                             break;
                             case 8:
-                                if(debug_info)
-                                    std::cout<<"increase size";
-                                code_runTime_copy.store.push_back(0);
+                                if(runTime_code_output)
+                                    last_runTime_code_p += "increase size";
+                                code_runTime_copy.push_back(0);
                                 if (code_pos == 0)
                                     code_pos = code_runTime_size;
                             break;
                             case 9:
                             {
-                                if(debug_info)
-                                    std::cout<<"increment value at p1";
-                                size_t v1 = code_runTime_copy.store[code_pos]
+                                if(runTime_code_output)
+                                    last_runTime_code_p += "increment @p1";
+                                size_t v1 = code_runTime_copy[code_pos]
                                     % code_runTime_size;
-                                code_runTime_copy.store[v1] =
-                                    (code_runTime_copy.store[v1] + 1) % USHRT_MAX;
+                                code_runTime_copy[v1] =
+                                    (code_runTime_copy[v1] + 1) % USHRT_MAX;
                                 code_pos++;
                             }
                             break;
                             case 10:
                             {
-                                if(debug_info)
-                                    std::cout<<"decrement value at p1";
-                                size_t v1 = code_runTime_copy.store[code_pos]
+                                if(runTime_code_output)
+                                    last_runTime_code_p += "decrement @p1";
+                                size_t v1 = code_runTime_copy[code_pos]
                                     % code_runTime_size;
-                                code_runTime_copy.store[v1] =
-                                    abs((code_runTime_copy.store[v1] - 1)
+                                code_runTime_copy[v1] =
+                                    abs((code_runTime_copy[v1] - 1)
                                     % USHRT_MAX);
                                 code_pos++;
                             }
                             break;
                         }
-                        if(debug_info)
-                            std::cout<<std::endl;
                         run_time_this_output++;
-                        code_runTime_size = code_runTime_copy.store.size();
+                        code_runTime_size = code_runTime_copy.size();
                         code_pos = code_pos % code_runTime_size;
                         if(run_time_this_output == most_run_time_per_output) {
                             cont = false;
-                            can_run_again_p = false;}
-                        if(code_runTime_size > largest_code_runTime_size)
-                        {
+                            ok_run = false;
+                            last_error = "out of time";}
+                        if(code_runTime_size > largest_code_runTime_size) {
                             cont = false;
-                            can_run_again_p = false;
-                            if (debug_info)
-                                std::cout<<"became too large"<<std::endl;
-                        }
+                            ok_run = false;
+                            last_error = "code too large";}
+                        if(runTime_code_output && cont)
+                            last_runTime_code_p += " ";
                     }
                     first_value = false;
+                    return ok_run;
                 }
 
             private:
-                VMs::newtAble::instructions
+                std::vector<int>
                     code,
                     code_runTime_copy;
-                bool can_run_again_p,
-                    debug_info,
-                    first_value;
-                unsigned short* latest_output_p;
+                bool first_value;
                 size_t code_pos;
+                std::string
+                    last_error,
+                    last_runTime_code_p;
+                unsigned int
+                    most_run_time_per_output,
+                    largest_code_runTime_size;
+                const static std::vector<std::string> instruction_names =
+                    // maybe this members initialisation can go into a static constructor instead
+                    {"end", "goto", "if", "=", "+", "-", "toggle",
+                        "output", "increase", "increment", "decrement"};
         };
     }
-}
-
-/* int main()
-{
-	srand((unsigned int)time(NULL));
-    interpreter a = interpreter();
-	a.random(50);
-	std::cout<<std::endl<<std::endl;
-	a.run(10, 100, 10, false);
-	std::cout<<std::endl<<std::endl;
-	a.run(10, 100, 10, false);
-	return 0;
-} */
-
-int main()
-{
-    return 0;
 }
